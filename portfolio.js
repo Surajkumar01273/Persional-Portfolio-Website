92db14dd0856b6f2f0be462e4ad40809204ac768
Suraj kumar <surajshk1995@gmail.com>
2024-05-14 20:41:07 +0530

    first version 1.0

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontants = document.getElementsByClassName('tab-contents');
+
+//  function opentab(tabname){
+//     for(tablink of tablinks){
+//         tablink.classList.remove("active-links")
+//     }
+
+//     for(tabcotant of tabcontants){
+//         tabcontant.classList.remove("active-tab")
+//     }
+//  }
